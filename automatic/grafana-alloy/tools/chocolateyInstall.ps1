2c4d6f2a5f936fd09020d2e395853ffb16d44ae5
Daniel Komaz <d.komaz@copsgmbh.com>
2024-11-22 23:40:31 +0100

    fix grafana-alloy installation script

--- automatic/grafana-alloy/tools/chocolateyInstall.ps1
+++ automatic/grafana-alloy/tools/chocolateyInstall.ps1
@@ -2,12 +2,14 @@
 
 $packageName = 'grafana-alloy'
 $url64 = 'https://github.com/grafana/alloy/releases/download/v1.5.0/alloy-installer-windows-amd64.exe.zip'
-$checksum64 = '59b2d8bd745d1fef88f04ed79133ee87789be6d796735aed5fe5e062e94458f7'
 $version = '1.5.0'
 $fileName = 'alloy-installer-windows-amd64.exe'
-# $unzipDestination = $env:Temp + '\' + $packageName + '\' + $version + '\'
 $unzipDestination = (Split-Path -Parent $MyInvocation.MyCommand.Definition)
+$checksum_download = 'https://github.com/grafana/alloy/releases/download/v1.5.0/SHA256SUMS'
+$checksum_content = Invoke-RestMethod -Uri $checksum_download
+$checksum = ($checksum_content -split "`n" | Where-Object { $_ -like "*$fileName" }) -split " " | Select-Object -First 1
 
+# Download the package
 $zipPackageArgs = @{
   packageName    = $packageName
   fileType       = 'exe'
@@ -16,27 +18,54 @@ $zipPackageArgs = @{
   silentArgs     = "/S"
   validExitCodes = @(0, 1223)
   softwareName   = 'Grafana Alloy'
-  checksum       = $checksum64
+  checksum       = $checksum
+  checksumType   = 'sha256'
+  checksum64     = $checksum
   checksumType64 = 'sha256'
   version        = $version
   unzipLocation  = $unzipDestination
 }
 Install-ChocolateyZipPackage @zipPackageArgs
-# Get-ChocolateyUnzip -FileFullPath $url64 -Destination $unzipDestination
 
-$packageArgs = @{
-  packageName    = $packageName
-  fileType       = 'exe'
-  url            = $unzipDestination + '\' + $fileName
-  url64bit       = $unzipDestination + '\' + $fileName
-  silentArgs     = "/S"
-  validExitCodes = @(0, 1223)
-  softwareName   = 'Grafana Alloy'
-  checksum       = $checksum64
-  checksumType   = 'sha256'
-  checksum64     = $checksum64
-  checksumType64 = 'sha256'
-  version        = $version
+# Validate the checksum of the downloaded file
+$downloadedFilePath = Join-Path $unzipDestination $fileName
+
+# Get the package parameters
+$pp = Get-PackageParameters
+Write-Host "Package parameters: $($pp)"
+
+if (!$pp.CONFIG) {
+  Write-Host "No configuration file provided. Using default configuration."
+  $pp.CONFIG = ""
+}
+else {
+  Write-Host "Using configuration file $($pp.CONFIG)."
 }
 
-Install-ChocolateyPackage @packageArgs
+if (!$pp.DISABLEREPORTING) { 
+  $pp.DISABLEREPORTING = "no"
+}
+else {
+  Write-Host "Disabling reporting."
+  $pp.DISABLEREPORTING = "yes"
+}
+
+if (!$pp.DISABLEPROFILING) {
+  $pp.DISABLEPROFILING = "no"
+}
+else {
+  Write-Host "Disabling profiling."
+  $pp.DISABLEPROFILING = "yes"
+}
+
+if (!$pp.ENVIRONMENT) {
+  $pp.ENVIRONMENT = ""
+}
+
+$silentArgs = "/S /DISABLEREPORTING=$($pp['DISABLEREPORTING']) /DISABLEPROFILING=$($pp['DISABLEPROFILING']) /CONFIG=$($pp['CONFIG']) /ENVIRONMENT=$($pp['ENVIRONMENT'])"
+
+Write-Host "Silent arguments: $($silentArgs)"
+
+$silentArgs = $silentArgs.Split()
+
+& "$downloadedFilePath" $silentArgs
