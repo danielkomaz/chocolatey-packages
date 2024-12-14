0bb7030b2eb8cd0c0624dcb91ba8269e45e8a824
Daniel Komaz <d.komaz@copsgmbh.com>
2024-12-14 21:04:13 +0100

    Fix setup of chocoProfile script

--- setup/chocoProfile.ps1
+++ setup/chocoProfile.ps1
@@ -8,9 +8,16 @@ if (Test-Path($ChocolateyProfile)) {
 
 $profileFile = "$profile"
 $chocoProfileSearch = '$ChocolateyProfile'
+if (Test-Path $profileFile) {
+  Write-Debug "Profile file exists."
+}
+else {
+  Write-Debug "Profile file does not exist. Creating it."
+  New-Item -Path $profileFile -ItemType File -Force | Out-Null
+}
 if (Select-String -Path $profileFile -Pattern $chocoProfileSearch -Quiet -SimpleMatch) {
   Write-Debug "Chocolatey profile is already installed."
   return
 }
 
-$profileInstall | Out-File $profileFile -Append -Encoding (Get-FileEncoding $profileFile)
+$profileInstall | Out-File $profileFile -Append
